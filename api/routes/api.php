c0e5f999d40d96d983607a2c3bb19acf089779ff
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2022-03-03 10:38:56 -0300

    Criando os endpoints da API no arquivo de rotas api

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ProductController::class)->prefix('v1')->group(function () {
+    Route::get('/products', 'index');
+    Route::post('/product', 'store');
+    Route::get('/product/{id}', 'show');
+    Route::put('/product/{id}', 'update');
+    Route::delete('/product/{id}', 'destroy');
 });
