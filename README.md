71949937214993b2fd18fed2c857a62b9c891112
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2022-03-03 15:20:32 -0300

    Implementando o CreateProduct e editando o README

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Projeto de CRUD com Laravel 9 (API) e React JS (Frontend)
+
+## Passo a passo ao clonar o projeto
+
+### API
+Acessar o diretório api
+  #### cd api
+  
+Configurar o acesso ao banco de dados
+  #### cp .env.example .env
+  Editar as configuraçãoes de acordo com o banco de dados a ser utilizado
+  
+Executar o composer install
+#### composer install
+
+Gerar a chave da aplicação
+#### php artisan key:generate
+
+Executar as migrations e os seeders
+#### php artisan migrate --seed
+
+Levantar o servidor localmente
+#### php artisan serve
+
+<hr />
+
+### Frontend
+
+Acessar o diretório frontend
+  #### cd frontend
+    
+Executar o npm install
+#### npm install
+
+Executar a aplicação localmente
+#### npm run dev
--- frontend/src/components/CreateProduct.jsx
+++ frontend/src/components/CreateProduct.jsx
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from  'react-router-dom';
 
 const CreateProduct = () => {
+
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState(0);
+  const [stock, setStock] = useState(0);
+
+  const navigate = useNavigate();
+
+  const store = async (event) => {
+    event.preventDefault();
+    await axios.post(`http://localhost:8000/api/v1/product`, {
+      description: description,
+      price: price,
+      stock: stock
+    });
+    navigate('/');
+  }
+
   return (
-    <div>CreateProduct</div>
+    <div>
+      <h3>Create Product</h3>
+      <form onSubmit={store}>
+        <div className="mb-3">
+          <label className="form-label">Description</label>
+          <input 
+            type="text" 
+            className="form-control"
+            value={description}
+            onChange={ (event) => setDescription(event.target.value) }
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Price</label>
+          <input 
+            type="number" 
+            className="form-control"
+            value={price}
+            onChange={ (event) => setPrice(event.target.value) }
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Stock</label>
+          <input 
+            type="number" 
+            className="form-control"
+            value={stock}
+            onChange={ (event) => setStock(event.target.value) }
+          />
+        </div>
+
+        <button type='submit' className="btn btn-sm btn-primary">Store</button>
+      </form>
+    </div>
   );
 }
 
