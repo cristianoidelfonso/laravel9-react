1264b0604b4437718c48e86562c3aad64351b8b5
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2022-03-04 10:34:58 -0300

    Criando as rotas de autenticação

--- api/routes/api.php
+++ api/routes/api.php
@@ -19,6 +19,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['auth' => 'auth', 'middleware' => 'api'], function ($router) {
+    Route::post('/login', \App\Http\Controllers\Auth\AuthController::class, 'login' );
+    Route::post('/logout', \App\Http\Controllers\Auth\AuthController::class, 'logout' );
+    Route::post('/refresh', \App\Http\Controllers\Auth\AuthController::class, 'refresh' );
+    Route::post('/me', \App\Http\Controllers\Auth\AuthController::class, 'me' );
+    Route::post('/register', \App\Http\Controllers\Auth\AuthController::class, 'register' );
+});
+
 Route::controller(ProductController::class)->prefix('v1')->group(function () {
     Route::get('/products', 'index');
     Route::post('/product', 'store');
