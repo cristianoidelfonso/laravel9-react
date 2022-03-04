9dac719b4b5bc947a9fa0dc4c60a356ef707ae84
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2022-03-04 08:55:39 -0300

    Customização de cores bootstrap SCSS

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/scss/custom.css';
 
 ReactDOM.render(
   <React.StrictMode>
