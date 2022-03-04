f42298b4eb6a788b5b164ef6e1597fbacf9573cc
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2022-03-04 11:54:42 -0300

    Implementando os métodos no AuthController e Refatorando as rotas de autenticação

--- api/app/Http/Controllers/Auth/AuthController.php
+++ api/app/Http/Controllers/Auth/AuthController.php
@@ -3,34 +3,79 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    public function __contruct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
 
     public function login()
     {
-        return 'Login';
+        $credentials = request(['email', 'password']);
+
+        if ( !$token = auth()->attempt($credentials) ) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
     }
 
 
     public function logout()
     {
-        return 'Logout';
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
     }
+
 
     public function refresh()
     {
-        return 'Refresh';
+        return $this->respondWithToken(auth()->refresh());
     }
+
 
     public function me()
     {
-        return 'Me';
+        return response()->json(auth()->user());
     }
 
-    public function register()
+    protected function respondWithToken($token)
     {
-        return  'Register';
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
     }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:2,100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create(array_merge(
+            $validator->validated(),
+            ['password' => bcrypt($request->password)]
+        ));
+
+        return response()->json([
+            'message' => 'User successfully registered',
+            'user' => $user
+        ], 201);
+    }
+
 }
